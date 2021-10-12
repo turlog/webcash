d313b8b883b3984d1a5c4dfacf93d436bddf0a6d
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2021-10-12 20:35:39 +0200

    minimalistic BitBay API client with pagination support for collections

--- scripts/bitbay.py
+++ scripts/bitbay.py
@@ -1,10 +1,8 @@
-import os
 import uuid
 import hmac
 import json
 from datetime import datetime
 import requests
-import logging
 
 
 class BitBay:
@@ -16,7 +14,7 @@ class BitBay:
 
     def auth_headers(self, query=None):
         timestamp = str(int(datetime.now().timestamp()))
-        message = self.__key + timestamp + (json.dumps(query) if query else '')
+        message = self.__key + timestamp + (json.dumps(query) if query is not None else '')
         signature = hmac.digest(self.__secret.encode('ascii'), message.encode('ascii'), 'sha512').hex()
         return {
             'API-Key': self.__key,
@@ -26,42 +24,24 @@ class BitBay:
         }
 
     def request(self, method, endpoint, query=None):
-        items = []
+        response = self.__session.request(
+            method=method,
+            json=query or {},
+            headers=self.auth_headers(query),
+            url=f'https://api.bitbay.net/rest/{endpoint}'
+        ).json()
+        if response.get('status') == 'Ok':
+            return response
+        else:
+            raise ValueError(response.pop('errors', ['UNKNOWN_ERROR']))
+
+    def collection(self, method, endpoint, query=None):
         query = query or {}
         query['nextPageCursor'] = 'start'
 
         while True:
-            response = self.__session.request(
-                method=method,
-                json=query or {},
-                headers=self.auth_headers(query),
-                url=f'https://api.bitbay.net/rest/{endpoint}'
-            ).json()
-            if response.get('status') == 'Ok':
-                items.extend(response['items'])
-                if query.get('nextPageCursor') == response.get('nextPageCursor'):
-                    break
-                query['nextPageCursor'] = response['nextPageCursor']
-            else:
-                raise ValueError(response.get('errors', ['UNKNOWN_ERROR']))
-
-        return items
-
-
-if __name__ == '__main__':
-
-    logging.basicConfig(level=logging.DEBUG)
-
-    bitbay = BitBay(os.environ.get('BITBAY_PUBLIC'), os.environ.get('BITBAY_PRIVATE'))
-
-    data = bitbay.request('POST', 'trading/history/transactions', {
-        'markets': ['BTC-PLN'],
-        'rateFrom': '1',
-        'rateTo': '99999',
-        'userAction': 'buy',
-        'fromTime': None,
-        'toTime': None,
-    })
-
-    print(json.dumps(data, indent=4))
-
+            response = self.request(method, endpoint, query)
+            yield from response['items']
+            if query.get('nextPageCursor') == response.get('nextPageCursor'):
+                break
+            query['nextPageCursor'] = response['nextPageCursor']
