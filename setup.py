3e8139afda1298d2f06913e48bfb7b7f04d5b033
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2022-06-18 10:43:19 +0200

    turned script into package, setup.py with entrypoint added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webcash',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    version='0.1',
+    python_requires='>=3.8',
+    install_requires=[
+        'tabulate',
+        'click',
+        'furl',
+        'ruamel.yaml',
+        'piecash',
+    ],
+    entry_points={
+        'console_scripts': [
+            'statements=webcash.utils.statements:cli'
+        ]
+    }
+)
